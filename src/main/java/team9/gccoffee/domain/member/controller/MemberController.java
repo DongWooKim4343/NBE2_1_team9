4c790cbb859cf455ea2c359a2b622e28348c7b75
sumin <grandcias123@gmail.com>
2024-09-10 10:41:36 +0900

    임시저장(..controller)

--- src/main/java/team9/gccoffee/domain/member/controller/MemberController.java
+++ src/main/java/team9/gccoffee/domain/member/controller/MemberController.java
@@ -1,11 +1,13 @@
 package team9.gccoffee.domain.member.controller;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import java.nio.file.Path;
 import java.util.Collection;
 import java.util.Map;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -17,6 +19,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import team9.gccoffee.domain.member.dto.MemberRequestDTO;
+import team9.gccoffee.domain.member.dto.MemberResponse;
+import team9.gccoffee.domain.member.dto.MemberUpdateDTO;
+import team9.gccoffee.domain.member.dto.PageRequestDTO;
 import team9.gccoffee.domain.member.service.MemberService;
 
 @RestController
@@ -29,36 +34,55 @@ public class MemberController {
 
     //등록
     @PostMapping
-    public ResponseEntity<MemberRequestDTO> register(
+    public ResponseEntity<MemberResponse> register(
             @Validated @RequestBody MemberRequestDTO memberRequestDTO) {
 
         //어떤 예외 처리 필요?
 
-        //return ResponseEntity.ok(memberService.createMember(memberRequestDTO));
-        return null;
+        return ResponseEntity.ok(memberService.createMember(memberRequestDTO));
     }
 
 
     //조회 ////////////
     //memberId 로 멤버 개별 조회
-    //@GetMapping("/{memberId}")
+    @GetMapping("/{memberId}")
+    public ResponseEntity<Member> read(
+            @PathVariable("memberId") Long memberId) {
 
+        return ResponseEntity.ok(memberService.getMemberById(memberId));
+    }
 
 
     //멤버 전체 조회
-    //@GetMapping
+    @GetMapping
+    public ResponseEntity<Page<Member>> getMemberList(
+            @Validated PageRequestDTO pageRequestDTO) {
+        return ResponseEntity.ok(memberService.getAllMembers(pageRequestDTO));
+
+    }
 
 
 
     //개인 주문 조회 myOrders/{memberId}
-    //@GetMapping("/myOrders/{memberId}")
-    //public ResponseEntity<>
+    @GetMapping("/myOrders/{memberId}")
+    public ResponseEntity<Member> getMemberOrderList(
+            @PathVariable("memberId") Long memberId) {
+        return ResponseEntity.ok(memberService.)
+    }
+
 
 
     ////////////
 
     //수정 memberId
-    //@PutMapping("/{memberId}")
+    @PutMapping("/{memberId}")
+    public ResponseEntity<MemberResponse> modify(
+            @Validated @RequestBody MemberUpdateDTO memberUpdateDTO,
+            @PathVariable("memberId") Long memberId ) {
+
+        return ResponseEntity.ok(memberService.updateMember(memberUpdateDTO));
+    }
+
 
 
 
